c9d9e54215d1dc3ea272c3161d99edf354ba9ed9
Tobias Crawley <tcrawley@gmail.com>
2012-11-20 09:11:22 -0500

    Add support for attributing guest authors.

--- _ext/format_helper.rb
+++ _ext/format_helper.rb
@@ -2,6 +2,13 @@ module FormatHelper
   def format_date( date )
     date.strftime( '%A, %B %e, %Y' ) if date
   end
+
+  def author( page )
+    author = page.author || "The Immutant Team"
+    if page.author_url
+      %Q{<a href="#{page.author_url}">#{author}</a>}
+    else
+      author
+    end
+  end
 end
-    
-    
